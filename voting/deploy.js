2a297da1a396797f4394f09b2d1dabab925a9632
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-09-23 03:49:46 +0800

    Updated deploy.js to patch bugs:     Currently still not working but got the setup of the solidity compiler     to be working and correct, as the first few versions were based on the     older versions of deploy scripts and web version.

--- voting/deploy.js
+++ voting/deploy.js
@@ -1,6 +1,9 @@
 /*  Script to deploy smart contract onto the test net
 */
 
+// const ganache = require("ganache-core");
+// const web3 = new Web3(ganache.provider());
+
 // Web3 library used to interface with the Testnet
 const Web3 = require('web3');
 // fs library needed to read the contract source file for compilation
@@ -10,14 +13,43 @@ const solc = require('solc')
 
 // Create a new connection interface
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const print = console.log;
 
-// Read the code as string before compiling
-code = fs.readFileSync('./src/voting.sol').toString();
-compiledCode = solc.compile(code);
 
-abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface)
-VotingContract = web3.eth.contract(abiDefinition)
-byteCode = compiledCode.contracts[':Voting'].bytecode
+const input = {
+    language: 'Solidity',
+    sources: {
+        'voting.sol': {
+            // Read the code as string before compiling
+            content: fs.readFileSync('./src/voting.sol').toString()
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+}
+
+const compiledCode = JSON.parse(solc.compile(JSON.stringify(input)))
+
+// `compiledCode` here contains the JSON compiledCode as specified in the documentation
+for (var contractName in compiledCode.contracts['voting.sol']) {
+    // console.log(contractName + ': ' + compiledCode.contracts['voting.sol'][contractName].evm.bytecode.object)
+    // console.log(': ' + compiledCode.contracts['voting.sol']['Voting'].evm.bytecode.object)
+}
+
+print("TP1")
+const abi = compiledCode.contracts['voting.sol']['Voting'].abi;
+print(typeof abi);
+
+VotingContract = new web3.eth.Contract(abi)
+
+byteCode = compiledCode.contracts['voting.sol'].bytecode;
+
+
 deployedContract = VotingContract.new(['Rama', 'Nick', 'Jose'], { data: byteCode, from: web3.eth.accounts[0], gas: 4700000 })
 deployedContract.address
 contractInstance = VotingContract.at(deployedContract.address)
